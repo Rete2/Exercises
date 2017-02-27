b9720cc6114d2165998e29cefa4fdf9fcc168e5a
Rete2 <rete@posteo.de>
2017-02-27 17:26:18 +0100

    update

--- enter_pin_3times.rb
+++ enter_pin_3times.rb
@@ -4,7 +4,7 @@ print "> "
 def pincheck
   pin_correct = 1234
   loopnr = 1
-  while (user_pin = gets.chomp.to_i) != pin_correct do        #do is not needed, to_i because pin_correct is an integer while gets.chomp gets a string
+  while (user_pin = gets.chomp.to_i) != pin_correct do 
     puts "This PIN was not correct. Please enter again"
     puts "Try: no. #{loopnr}."
     if loopnr == 3

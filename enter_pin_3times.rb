27605cbc28aab80ce94b9062aed4ed8073b47129
Rete2 <rete@posteo.de>
2017-02-27 17:51:43 +0100

    update

--- enter_pin_3times.rb
+++ enter_pin_3times.rb
@@ -4,14 +4,13 @@ print "> "
 def pincheck
   pin_correct = 1234
   loopnr = 1
-  while (user_pin = gets.chomp.to_i) != pin_correct do 
+  while (user_pin = gets.chomp.to_i) != pin_correct do
     puts "This PIN was not correct. Please enter again"
     puts "Try: no. #{loopnr}."
     if loopnr == 3
       break
     end
     loopnr += 1
-
   end
 
 if user_pin == pin_correct
@@ -19,7 +18,6 @@ if user_pin == pin_correct
 else
   puts "This was your last try"
 end
-
 end
 
 pincheck
